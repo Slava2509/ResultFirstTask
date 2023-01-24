06c029d4bc9368ae54abc2c32129cd98e0c59ecf
geekbrains <info@geekbrains>
2023-01-24 23:53:35 +0500

    добавил программу

---
+++ result1/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] array = new string[5] { "Russia", "leo", "hello", "0-", ":)s" };
+string[] arrEl = new string[array.Length];
+void ArrayWithThreeEl(string[] arr1, string[] arr2)
+{
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[i] = arr1[i];
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayWithThreeEl(array, arrEl);
+PrintArray(arrEl);
+
